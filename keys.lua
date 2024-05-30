5b49c17edb93924a2e9c4d588bd088b2d53d726a
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-05-30 17:50:51 -0400

    Removing expired whitelist entries

--- keys.lua
+++ keys.lua
@@ -1,5 +1,4 @@
 return {
 "free", --UserId = "123", --expiry_date = "2080-05-14 23:41:31"
-"JSbk4", --UserId = "123", --expiry_date = "2024-05-30 17:50:30"
-"vqfU8", --UserId = "123", --expiry_date = "2024-05-30 17:50:33"
 }
+
