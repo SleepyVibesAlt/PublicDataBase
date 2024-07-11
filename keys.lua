c9267628f8b8fe3ac632c9e9f68b291b533bda5f
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-11 19:52:38 -0400

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,18 +1,5 @@
 return {
     ["Whitelist Data"] = {
         -- Add your key and HWID pairs here as needed
-        {
-            Key = "your_generated_key",
-            Hwid = "your_generated_hwid"
-        }
     }
-,
-{
-    Key = ray81bor6alrynt3w8ll3c,
-    Hwid = 2342
-}}
-
-{
-    Key = vvaqoh9xr4c5vaqo57lie
-    Hwid = 2342
 }
