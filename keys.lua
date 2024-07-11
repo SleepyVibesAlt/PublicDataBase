40416989cd826dfe781196f4f51b22b62b384725
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-11 18:33:37 -0400

    Add new key and URL

--- keys.lua
+++ keys.lua
@@ -6,3 +6,5 @@ glrywg6wlf3mhgjnrk5ie
 https://067d8aa1-9111-4e19-a5fa-fc805f72c9d3-00-3u30icwo8n2pj.worf.replit.dev/?hwid=ldmrq8c6w0fhz3lc4djwbc
 ggh6s48kiol0vex1vqdpfw
 https://067d8aa1-9111-4e19-a5fa-fc805f72c9d3-00-3u30icwo8n2pj.worf.replit.dev/?hwid=4cyibhd93d3uixobipqbf
+079uu90lu8r38e6jty9gx16
+https://067d8aa1-9111-4e19-a5fa-fc805f72c9d3-00-3u30icwo8n2pj.worf.replit.dev/?hwid=onatyquj0jtn2uuapcp61
