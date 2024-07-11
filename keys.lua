fab62b82670060baa0bccf428f9bbb6db7294cfd
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-11 19:05:30 -0400

    Add new key and HWID

--- keys.lua
+++ keys.lua
@@ -2,3 +2,9 @@ return {
     ["Whitelist Data"] = {
     }
 }
+
+                {
+                    Key = evjxqmtpk6ro6zbcagbc8
+                    Hwid = 2342
+                }
+                                   
