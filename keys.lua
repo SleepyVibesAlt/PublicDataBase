c3e3c573953631575f63b85fcc5beed20d0228ee
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-11 20:07:01 -0400

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,13 +1,8 @@
 return {
     ["Whitelist Data"] = {
-    }
-
-    Key = "y3ednmlpt8o349e05s2df6",
-    Hwid = "2342"
-
-
         {
-            Key = "iz3moyqm9rjbx6d2xnq9bi",
-            Hwid = "2342"
+            Key = "60cf9a06-bc62-4797-afa5-1687baa106e8",
+            Hwid = "2BB9C63A-7391-4263-986A-DD7CF02328A9",
         },
-        }
+    }
+}
