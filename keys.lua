da1bf456ec123a2afb40c539662fc7a003832761
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-11 20:07:05 -0400

    Add new key and HWID

--- keys.lua
+++ keys.lua
@@ -4,5 +4,11 @@ return {
             Key = "60cf9a06-bc62-4797-afa5-1687baa106e8",
             Hwid = "2BB9C63A-7391-4263-986A-DD7CF02328A9",
         },
-    }
+    
+
+        {
+            Key = "lq70tabeqaayjgewbex5q",
+            Hwid = "2342"
+        },
+        }
 }
