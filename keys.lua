bfa9e701cd221e04ac334b0cd7ac0a083df4a512
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-11 20:03:03 -0400

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,7 +1,7 @@
 return {
     ["Whitelist Data"] = {
-    
-    Key = "gfxg52mfxy8o0kc30kjg8j",
-    Hwid = "2342"
-}
+    }
+        Key = "gfxg52mfxy8o0kc30kjg8j",
+        Hwid = "2342"
+    }
 }
